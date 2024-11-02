cf4ca206dcbc7c12e03e049094e484490af8792d
lucaf8664 <36150186+Lucaf129@users.noreply.github.com>
2024-11-02 16:10:56 +0100

    aggiunti altri scafolded items

---
+++ OpenCRMOpt/OpenCRMOpt/Migrations/20241102143717_InitialCreate.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenCRMOptModels.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Macchine",
+                columns: table => new
+                {
+                    MacchineID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descrizione = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IP = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Macchine", x => x.MacchineID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ModelliLotti",
+                columns: table => new
+                {
+                    ModelloID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descrizione = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MacchineCompatibili = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelliLotti", x => x.ModelloID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lotti",
+                columns: table => new
+                {
+                    LottoID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modello = table.Column<int>(type: "int", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantita = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lotti", x => x.LottoID);
+                    table.ForeignKey(
+                        name: "FK_Lotti_Modelli",
+                        column: x => x.Modello,
+                        principalTable: "ModelliLotti",
+                        principalColumn: "ModelloID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lotti_Modello",
+                table: "Lotti",
+                column: "Modello");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lotti");
+
+            migrationBuilder.DropTable(
+                name: "Macchine");
+
+            migrationBuilder.DropTable(
+                name: "ModelliLotti");
+        }
+    }
+}
---
+++ OpenCRMOpt/OpenCRMOptAPI/Controllers/MacchineController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OpenCRMOptModels;
+
+namespace OpenCRMOptAPI.Controllers
+{
+    public class MacchineController : Controller
+    {
+        private readonly OptDbContext _context;
+
+        public MacchineController(OptDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Macchine
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Macchines.ToListAsync());
+        }
+
+        // GET: Macchine/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var macchine = await _context.Macchines
+                .FirstOrDefaultAsync(m => m.MacchineId == id);
+            if (macchine == null)
+            {
+                return NotFound();
+            }
+
+            return View(macchine);
+        }
+
+        // GET: Macchine/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Macchine/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MacchineId,Descrizione,Name,Ip")] Macchine macchine)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(macchine);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(macchine);
+        }
+
+        // GET: Macchine/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var macchine = await _context.Macchines.FindAsync(id);
+            if (macchine == null)
+            {
+                return NotFound();
+            }
+            return View(macchine);
+        }
+
+        // POST: Macchine/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MacchineId,Descrizione,Name,Ip")] Macchine macchine)
+        {
+            if (id != macchine.MacchineId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(macchine);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MacchineExists(macchine.MacchineId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(macchine);
+        }
+
+        // GET: Macchine/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var macchine = await _context.Macchines
+                .FirstOrDefaultAsync(m => m.MacchineId == id);
+            if (macchine == null)
+            {
+                return NotFound();
+            }
+
+            return View(macchine);
+        }
+
+        // POST: Macchine/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var macchine = await _context.Macchines.FindAsync(id);
+            if (macchine != null)
+            {
+                _context.Macchines.Remove(macchine);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MacchineExists(int id)
+        {
+            return _context.Macchines.Any(e => e.MacchineId == id);
+        }
+    }
+}
