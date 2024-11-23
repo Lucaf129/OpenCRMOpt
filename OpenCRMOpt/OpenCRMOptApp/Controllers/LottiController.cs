81c64902c50be5fc897099b9c4bc7e79a342add9
lucaf8664 <36150186+Lucaf129@users.noreply.github.com>
2024-11-23 11:54:32 +0100

    aggiunto progetto

---
+++ OpenCRMOpt/OpenCRMOptApp/Controllers/LottiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace OpenCRMOptApp.Controllers
+{
+    public class LottiController : Controller
+    {
+
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+
+        public LottiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var httpClient = _httpClientFactory.CreateClient();
+
+            var httpResponseMessage = await httpClient.GetAsync("http://localhost:5055/api/Lotti");
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+
+                
+            }
+
+
+            return View();
+        }
+    }
+}
