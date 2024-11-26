9c9114b617c046a2a2af7054fc4a08b77c96af63
lucaf8664 <36150186+Lucaf129@users.noreply.github.com>
2024-11-26 17:41:04 +0100

    iniziata pagina ottimizzazione

---
+++ OpenCRMOpt/OpenCRMOptApp/Controllers/OttimizzazioneController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenCRMOptApp.Controllers
+{
+    public class OttimizzazioneController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult OttimizzaNaive()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OttimizzaConEuristica()
+        {
+            return View();
+        }
+    }
+}
