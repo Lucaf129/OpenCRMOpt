2a548d4f278fa48d84ee484a2f5b8d917d6f1057
lucaf8664 <36150186+Lucaf129@users.noreply.github.com>
2024-12-03 16:28:43 +0100

    creata metodo get matrice lotti macchine

--- OpenCRMOpt/OpenCRMOptAPI/Ottimizzazione/Ottimizzatore.cs
+++ OpenCRMOpt/OpenCRMOptAPI/Ottimizzazione/Ottimizzatore.cs
@@ -16,5 +16,40 @@ namespace OpenCRMOptAPI.Ottimizzazione
             return null;
         }
 
+        public List<List<bool>> getMatriceLottiMacchine(List<ModelliLotti> modelliLotti)
+        {
+            var matriceLottiMacchine = new List<List<bool>>();
+            var listLottiMacchine = new List<bool>();
+
+            foreach (var modelli in modelliLotti) 
+            {
+                listLottiMacchine = getListaLottiMacchine(modelli);
+                matriceLottiMacchine.Add(listLottiMacchine);
+            }
+
+            return matriceLottiMacchine;
+
+        }
+
+        private List<bool> getListaLottiMacchine(ModelliLotti modelli)
+        {
+            var listLottiMacchine = new List<bool>();
+            var assegnamenti = modelli.MacchineCompatibili.Split(';');
+
+            foreach(var assegnamento in assegnamenti)
+            {
+                if(assegnamento == "1")
+                {
+                    listLottiMacchine.Add(true);
+                }
+                else
+                {
+                    listLottiMacchine.Add(false);
+                }
+            }
+
+            return listLottiMacchine;
+
+        }
     }
 }
--- OpenCRMOpt/OpenCRMOptTest/OttimizzatoreTest.cs
+++ OpenCRMOpt/OpenCRMOptTest/OttimizzatoreTest.cs
@@ -1,11 +1,54 @@
 using Newtonsoft.Json;
 using OpenCRMOptAPI.Ottimizzazione;
+using OpenCRMOptModels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace OpenCRMOptTest
 {
     [TestClass]
     public class OttimizzatoreTest
     {
+
+        [TestMethod]
+        public void GetMatriceLottiMacchine()
+        {
+            var serializedInput = 
+                "[{'modelloId':1,'descrizione':'Modello A','macchineCompatibili':'1;0;0;1;0','lottis':[]}," +
+                "{'modelloId':2,'descrizione':'Modello B','macchineCompatibili':'0;0;1;0;1','lottis':[]}, "+
+                "{'modelloId':3,'descrizione':'Modello C','macchineCompatibili':'0;1;0;0;0','lottis':[]},"+
+                "{'modelloId':4,'descrizione':'Modello D','macchineCompatibili':'1;0;1;0;0','lottis':[]}, "+
+                "{'modelloId':5,'descrizione':'Modello E','macchineCompatibili':'0;0;0;1;0','lottis':[]}, "+
+                "{'modelloId':6,'descrizione':'Modello F','macchineCompatibili':'0;0;1;1;0','lottis':[]}]";
+            var ottimizzatore = new Ottimizzatore();
+
+            // deserializzo la stringa
+            var modelliLottiList = JsonConvert.DeserializeObject<List<ModelliLotti>>(serializedInput);
+
+            var expected = new List<List<bool>>() {
+                new List<bool>(){true, false, false, true, false},
+                new List<bool>(){false, false, true, false, true},
+                new List<bool>(){false, true, false, false, false},
+                new List<bool>(){true, false, true, false, false},
+                new List<bool>(){false, false, false, true, false},
+                new List<bool>(){false, false, true, true, false} };
+
+
+            if (modelliLottiList != null)
+            {
+                var result = ottimizzatore.getMatriceLottiMacchine(modelliLottiList);
+
+                int i = 0;
+                foreach (var list in expected)
+                {
+                    CollectionAssert.AreEqual(list, result[i]);
+                    i++;
+                }
+                
+            }
+
+        }
+
+
         [TestMethod]
         public void OttimizzazioneConEuristica()
         {
@@ -21,11 +64,19 @@ namespace OpenCRMOptTest
                 "{'LottoId':9,'Quantita':1200,'Descrizione':'Modello D','MacchineCompatibili':'1;0;1;0;0'}]";
             var ottimizzatore = new Ottimizzatore();
 
+            // deserializzo la stringa
+            var lottiMacchineList = JsonConvert.DeserializeObject<List<LottiMacchine>>(serializedInput);
 
-            
+
+            if (lottiMacchineList != null)
+            {
+                RisultatoOttimizzazione result = ottimizzatore.OttimizzaConEuristica(lottiMacchineList);
 
 
-            ottimizzatore.OttimizzaConEuristica();
+            }
+
+
+
         }
     }
 }
