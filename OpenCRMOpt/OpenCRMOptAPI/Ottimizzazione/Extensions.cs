6606a72c8b9db71371e05f021fdccceeab139d0d
lucaf8664 <36150186+Lucaf129@users.noreply.github.com>
2024-12-13 18:41:32 +0100

    aggiunta estensione con il prodotto cartesiano

---
+++ OpenCRMOpt/OpenCRMOptAPI/Ottimizzazione/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenCRMOptAPI.Ottimizzazione
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+            IEnumerable<IEnumerable<T>> result = emptyProduct;
+            foreach (IEnumerable<T> sequence in sequences)
+            {
+                result = from accseq in result from item in sequence select accseq.Concat(new[] { item });
+            }
+            return result;
+        }
+    }
+}
